00c78ca440fd67efc2ae72753a9345c0c20f2ebf
S-rk-11 <shivanikalghatgi@gmail.com>
2025-06-30 20:51:23 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 import streamlit as st
 import pickle
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-# Load the saved pipeline
+# Required for first-time use
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# Text Preprocessing
+stop_words = set(stopwords.words('english'))
+lemma = WordNetLemmatizer()
+
+def clean_text(text):
+    text = re.sub('[^a-zA-Z:]', ' ', text)
+    text = text.lower()
+    words = text.split()
+    words = [lemma.lemmatize(w) for w in words if w not in stop_words]
+    return ' '.join(words)
+
+# Load pipeline
 with open('disease_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
@@ -9,7 +28,6 @@ with open('disease_model.pkl', 'rb') as file:
 st.set_page_config(page_title="Drug Condition Predictor", layout="centered")
 st.title("Drug Condition Predictor")
 
-# User input
 user_input = st.text_area("Enter the patient's drug review here:")
 
 if st.button("Predict Condition"):
